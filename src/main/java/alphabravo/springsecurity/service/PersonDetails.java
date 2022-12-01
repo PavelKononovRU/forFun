cf058d77a8bf6e9aa5d33be418fa21720d52fd35
Pavel <lovecars527@gmail.com>
2022-12-01 12:13:38 +0300

    work on edge Admin

---
+++ src/main/java/alphabravo/springsecurity/service/PersonDetails.java
@@ -0,0 +1,9 @@
+package alphabravo.springsecurity.service;
+
+import alphabravo.springsecurity.model.Person;
+import org.springframework.stereotype.Component;
+
+
+public interface PersonDetails {
+void savePerson(Person person);
+}
